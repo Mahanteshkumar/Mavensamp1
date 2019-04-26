3e67df913674c5b78af47db5abe5f1c878eed736
Mahanteshkuar <kumarmanthi@gmail.com>
2019-04-26 12:35:13 +0530

    moven first commit

---
+++ src/testscript/Simplesel.java
@@ -0,0 +1,33 @@
+package testscript;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+public class Simplesel {
+	public WebDriver driver;
+	@BeforeMethod
+	public void baseopp() {
+		System.setProperty("webdriver.gecko.driver","./exef/geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.seleniumhq.org/");
+	}
+	@Test
+	public void testcase1() {
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//li[@id='menu_download']")).click();
+		System.out.println(driver.getTitle());
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+}
